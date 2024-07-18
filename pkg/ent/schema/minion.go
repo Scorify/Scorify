6c1cf6547a3040cab7c423ce8d7eb374df74ed42
1nv8rZim <msf9542@rit.edu>
2024-07-17 22:11:25 -0400

    Add minion schema

---
+++ pkg/ent/schema/minion.go
@@ -0,0 +1,44 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+	"github.com/google/uuid"
+)
+
+// Minion holds the schema definition for the Minion entity.
+type Minion struct {
+	ent.Schema
+}
+
+// Fields of the Minion.
+func (Minion) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			StructTag(`json:"id"`).
+			Comment("The uuid of the minion").
+			Unique().
+			Immutable(),
+		field.String("name").
+			StructTag(`json:"name"`).
+			Comment("The name of the minion"),
+	}
+}
+
+// Indexes of the Minion.
+func (Minion) Indexes() []ent.Index {
+	return []ent.Index{}
+}
+
+// Mixins of the Minion.
+func (Minion) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		mixin.Time{},
+	}
+}
+
+// Edges of the Minion.
+func (Minion) Edges() []ent.Edge {
+	return []ent.Edge{}
+}
