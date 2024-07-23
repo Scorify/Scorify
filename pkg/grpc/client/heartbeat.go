058f5a072719f0ff2402f3e2ec9043869e420f4d
1nv8rZim <msf9542@rit.edu>
2024-07-22 21:09:17 -0400

    Add metrics submission to minion

--- pkg/grpc/client/heartbeat.go
+++ pkg/grpc/client/heartbeat.go
@@ -2,17 +2,37 @@ package client
 
 import (
 	"context"
+	"encoding/json"
 	"time"
 
 	"github.com/scorify/scorify/pkg/grpc/proto"
+	"github.com/scorify/scorify/pkg/structs"
 	"github.com/sirupsen/logrus"
 )
 
 func (c *MinionClient) Heartbeat(ctx context.Context) error {
 	start := time.Now()
 
-	_, err := c.client.Heartbeat(ctx, &proto.HeartbeatRequest{
+	// TODO: handle getting metrics
+
+	metrics := structs.MinionMetrics{
+		MinionID:    c.MinionID,
+		MemoryUsage: 0,
+		MemoryTotal: 0,
+		CPUUsage:    0,
+		CPUTotal:    0,
+		DiskUsage:   0,
+		DiskTotal:   0,
+	}
+
+	metrics_out, err := json.Marshal(metrics)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.client.Heartbeat(ctx, &proto.HeartbeatRequest{
 		MinionId: c.MinionID.String(),
+		Metrics:  string(metrics_out),
 	})
 	if err != nil {
 		return err
