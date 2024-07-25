6dd367c1d838b3f9bc583ca6679d72cbbb4fd589
1nv8rZim <msf9542@rit.edu>
2024-07-24 20:32:48 -0400

    feat: Update MinionMetrics struct to include CPU usage as a float
    The MinionMetrics struct in `minion_metrics.go` has been updated to include CPU usage as a float instead of an integer. This change allows for more precise measurement of CPU usage.

--- pkg/structs/minion_metrics.go
+++ pkg/structs/minion_metrics.go
@@ -1,6 +1,7 @@
 package structs
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
@@ -12,8 +13,40 @@ type MinionMetrics struct {
 	IP          string    `json:"ip"`
 	MemoryUsage int64     `json:"memory_usage"`
 	MemoryTotal int64     `json:"memory_total"`
-	CPUUsage    int64     `json:"cpu_usage"`
-	CPUTotal    int64     `json:"cpu_total"`
-	DiskUsage   int64     `json:"disk_usage"`
-	DiskTotal   int64     `json:"disk_total"`
+	CPUUsage    float64   `json:"cpu_usage"`
+	Goroutines  int64     `json:"goroutines"`
+}
+
+func (m MinionMetrics) String() string {
+	return fmt.Sprintf("minionID=%s, timestamp=%s, ip=%s, memoryUsage=%s, memoryTotal=%s, cpuUsage=%.2f%%, goroutines=%d",
+		m.MinionID,
+		m.Timestamp,
+		m.IP,
+		readableBytes(m.MemoryUsage),
+		readableBytes(m.MemoryTotal),
+		m.CPUUsage,
+		m.Goroutines,
+	)
+}
+
+const (
+	KB = 1024
+	MB = KB * 1024
+	GB = MB * 1024
+	TB = GB * 1024
+)
+
+func readableBytes(bytes int64) string {
+	bytes = bytes * 1024
+
+	if bytes < KB {
+		return fmt.Sprintf("%d B", bytes)
+	}
+	if bytes < MB {
+		return fmt.Sprintf("%.2f KB", float64(bytes)/KB)
+	}
+	if bytes < GB {
+		return fmt.Sprintf("%.2f MB", float64(bytes)/MB)
+	}
+	return fmt.Sprintf("%.2f GB", float64(bytes)/TB)
 }
