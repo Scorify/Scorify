e60947c1f19e326e22fa97218bd220bab2ef0e61
1nv8rZim <msf9542@rit.edu>
2024-07-31 21:32:44 -0400

    Add agents to router

--- src/Router.tsx
+++ src/Router.tsx
@@ -14,6 +14,7 @@ import {
 import { useAuth } from "./hooks";
 import {
   AdminChecks,
+  Agents,
   AdminPanel,
   ChangePassword,
   Home,
@@ -139,6 +140,10 @@ export function Router({ theme, setTheme, apolloClient }: props) {
             {
               path: "injects",
               element: <LazyComponent element={<Injects />} />,
+            },
+            {
+              path: "agents",
+              element: <LazyComponent element={<Agents />} />,
             },
           ],
         },
