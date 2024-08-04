d8655a8d0d78c235b5a7a32c21062732b967c613
1nv8rZim <msf9542@rit.edu>
2024-08-03 22:32:44 -0400

    Add minion metrics caching functionality

---
+++ pkg/cache/minion_metrics.go
@@ -0,0 +1,29 @@
+package cache
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/scorify/scorify/pkg/structs"
+)
+
+const (
+	// MinionStatusChannel is the key for the minion metrics pub/sub in redis
+	MinionStatusChannel = "minion_metrics_channel"
+)
+
+func PublishMinionMetrics(ctx context.Context, redisClient *redis.Client, metrics *structs.MinionMetrics) (*redis.IntCmd, error) {
+	out, err := json.Marshal(metrics)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := redisClient.Publish(ctx, MinionStatusChannel, string(out))
+
+	return resp, resp.Err()
+}
+
+func SubscribeMiniontMetrics(ctx context.Context, redisClient *redis.Client) *redis.PubSub {
+	return redisClient.Subscribe(ctx, MinionStatusChannel)
+}
