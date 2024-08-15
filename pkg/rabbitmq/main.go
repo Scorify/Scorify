a2d971f0975150d23578df9567695099dd31661a
1nv8rZim <msf9542@rit.edu>
2024-08-14 20:53:22 -0400

    Refactor RabbitMQ integration and server connection

--- pkg/rabbitmq/main.go
+++ pkg/rabbitmq/main.go
@@ -1,6 +1,7 @@
 package rabbitmq
 
 import (
+	"context"
 	"fmt"
 
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -8,7 +9,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func Serve() {
+func openClient() (*amqp.Connection, error) {
 	connStr := fmt.Sprintf(
 		"amqp://%s:%s@%s:%d/",
 		config.RabbitMQ.User,
@@ -19,59 +20,31 @@ func Serve() {
 
 	conn, err := amqp.Dial(connStr)
 	if err != nil {
-		logrus.WithError(err).Fatal("failed to connect to RabbitMQ")
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func Serve(ctx context.Context) error {
+	conn, err := openClient()
+	if err != nil {
+		return err
 	}
 	defer conn.Close()
 
-	ch, err := conn.Channel()
-	if err != nil {
-		logrus.WithError(err).Fatal("failed to open a channel")
-	}
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"hello",
-		false,
-		false,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		logrus.WithError(err).Fatal("failed to declare a queue")
-	}
-
-	err = ch.Publish(
-		"",
-		q.Name,
-		false,
-		false,
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte("Hello World"),
-		},
-	)
-	if err != nil {
-		logrus.WithError(err).Fatal("failed to publish a message")
-	}
-
-	msgs, err := ch.Consume(
-		q.Name,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		logrus.WithError(err).Fatal("failed to consume a message")
-	}
-
-	for msg := range msgs {
-		logrus.Infof("Received a message: %s", msg.Body)
-	}
-
 	logrus.Info("RabbitMQ server started")
+
+	go func() {
+		err := ListenSubmitTask(conn, ctx)
+		if err != nil {
+			logrus.WithError(err).Error("failed to listen submit task")
+		}
+	}()
+
 	select {}
+}
+
+func Client() (*amqp.Connection, error) {
+	return openClient()
 }
