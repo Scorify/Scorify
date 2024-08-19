73c60ed8cf4e5cf88a85c6624bc8ac2ebd1dcbe9
1nv8rZim <msf9542@rit.edu>
2024-08-18 22:26:22 -0400

    Add VhostsClient and List method for RabbitMQ management

---
+++ pkg/rabbitmq/management/vhosts/list.go
@@ -0,0 +1,39 @@
+package vhosts
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/scorify/scorify/pkg/rabbitmq/management/types"
+)
+
+func (c *VhostsClient) List() ([]*vhostsResponse, *types.ErrorResponse, error) {
+	url := fmt.Sprintf("%s/api/vhosts", c.host)
+
+	resp, err := c.httpClient.Get(url)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// fmt.Println(resp.StatusCode)
+	// io.Copy(os.Stdout, resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		var errorResponse types.ErrorResponse
+		err := json.NewDecoder(resp.Body).Decode(&errorResponse)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		return nil, &errorResponse, nil
+	}
+
+	var vhosts []*vhostsResponse
+	err = json.NewDecoder(resp.Body).Decode(&vhosts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return vhosts, nil, nil
+}
