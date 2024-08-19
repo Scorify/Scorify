144ceb133bcc426855f8fe3ae0a3e10fcf67da64
1nv8rZim <msf9542@rit.edu>
2024-08-18 21:04:49 -0400

    implement delete permissions endpoints

---
+++ pkg/rabbitmq/management/permissions/delete.go
@@ -0,0 +1,39 @@
+package permissions
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/scorify/scorify/pkg/rabbitmq/management/types"
+)
+
+func (c *PermissionsClient) Delete(user string, vhost string) (*types.ErrorResponse, error) {
+	escapedUser := url.QueryEscape(user)
+	escapedVhost := url.QueryEscape(vhost)
+
+	url := fmt.Sprintf("%s/api/permissions/%s/%s", c.host, escapedVhost, escapedUser)
+
+	req, err := http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusNoContent {
+		var errorResponse types.ErrorResponse
+		err = json.NewDecoder(resp.Body).Decode(&errorResponse)
+		if err != nil {
+			return nil, err
+		}
+
+		return &errorResponse, nil
+	}
+
+	return nil, nil
+}
