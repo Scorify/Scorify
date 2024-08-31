091874234efec68812703cbf2649f584e1d65026
1nv8rZim <msf9542@rit.edu>
2024-08-30 21:53:16 -0400

    refactor minion metrics to use heartbeat terminology

--- pkg/cache/minion.go
+++ pkg/cache/minion.go
@@ -13,37 +13,37 @@ import (
 )
 
 const (
-	// minion_metrics_key_prefix is the prefix for minion metrics
-	minion_metrics_key_prefix = "minion_metrics"
+	// minion_heartbeat_key_prefix is the prefix for minion heartbeats
+	minion_heartbeat_key_prefix = "minion_heartbeat"
 )
 
-func GetMinionMetrics(ctx context.Context, minionID uuid.UUID, redisClient *redis.Client) *structs.MinionMetrics {
-	data, err := redisClient.Get(ctx, fmt.Sprintf("%s:%s", minion_metrics_key_prefix, minionID.String())).Result()
+func GetMinionHeartbeat(ctx context.Context, minionID uuid.UUID, redisClient *redis.Client) *structs.Heartbeat {
+	data, err := redisClient.Get(ctx, fmt.Sprintf("%s:%s", minion_heartbeat_key_prefix, minionID.String())).Result()
 	if err != nil {
 		return nil
 	}
 
-	var minionMetrics structs.MinionMetrics
+	var minionHeartbeat structs.Heartbeat
 
-	err = json.Unmarshal([]byte(data), &minionMetrics)
+	err = json.Unmarshal([]byte(data), &minionHeartbeat)
 	if err != nil {
 		return nil
 	}
 
-	return &minionMetrics
+	return &minionHeartbeat
 }
 
-func SetMinionMetrics(ctx context.Context, minionID uuid.UUID, redisClient *redis.Client, metrics *structs.MinionMetrics) error {
+func SetMinionHeartbeat(ctx context.Context, minionID uuid.UUID, redisClient *redis.Client, metrics *structs.Heartbeat) error {
 	data, err := json.Marshal(metrics)
 	if err != nil {
 		return err
 	}
 
-	return redisClient.Set(ctx, fmt.Sprintf("%s:%s", minion_metrics_key_prefix, minionID.String()), data, 0).Err()
+	return redisClient.Set(ctx, fmt.Sprintf("%s:%s", minion_heartbeat_key_prefix, minionID.String()), data, 0).Err()
 }
 
-func GetMinionMetricsGroup(ctx context.Context, redisClient *redis.Client) ([]*structs.MinionMetrics, error) {
-	keys, err := redisClient.Keys(ctx, fmt.Sprintf("%s:*", minion_metrics_key_prefix)).Result()
+func GetMinionHeartbeatGroup(ctx context.Context, redisClient *redis.Client) ([]*structs.Heartbeat, error) {
+	keys, err := redisClient.Keys(ctx, fmt.Sprintf("%s:*", minion_heartbeat_key_prefix)).Result()
 	if err != nil {
 		return nil, err
 	}
@@ -53,20 +53,20 @@ func GetMinionMetricsGroup(ctx context.Context, redisClient *redis.Client) ([]*s
 		return nil, err
 	}
 
-	return static.MapSlice(data, func(_ int, value interface{}) *structs.MinionMetrics {
+	return static.MapSlice(data, func(_ int, value interface{}) *structs.Heartbeat {
 		data, ok := value.(string)
 		if !ok {
 			logrus.WithField("value", value).Error("failed to convert value to string")
 			return nil
 		}
 
-		var minionMetrics structs.MinionMetrics
-		err := json.Unmarshal([]byte(data), &minionMetrics)
+		var minionHeartbeat structs.Heartbeat
+		err := json.Unmarshal([]byte(data), &minionHeartbeat)
 		if err != nil {
 			logrus.WithError(err).Error("failed to unmarshal data")
 			return nil
 		}
 
-		return &minionMetrics
+		return &minionHeartbeat
 	}), nil
 }
