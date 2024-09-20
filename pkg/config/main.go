7fb86707130208605df1fd8ab047b9ca1dba41f9
1nv8rZim <msf9542@rit.edu>
2024-09-19 21:47:43 -0400

    remove configuration for GRPC

--- pkg/config/main.go
+++ pkg/config/main.go
@@ -63,18 +63,6 @@ var (
 		Password string
 	}
 
-	// gRPC is the configuration for the gRPC server
-	GRPC struct {
-		// Host is the host of the gRPC server
-		Host string
-
-		// Port is the port of the gRPC server
-		Port int
-
-		// Secret is the secret key for the gRPC server
-		Secret string
-	}
-
 	// Minion is the configuration for the minion
 	Minion struct {
 		// id is the id of the minion
@@ -118,7 +106,6 @@ func InitMinion() {
 	domain()
 	port()
 	interval()
-	grpc()
 	minionID()
 	rabbitmqMinion()
 }
@@ -135,7 +122,6 @@ func InitServer() {
 	jwt()
 	postgres()
 	redis()
-	grpc()
 	rabbitmqServer()
 }
 
@@ -231,25 +217,6 @@ func redis() {
 	Redis.Password = os.Getenv("REDIS_PASSWORD")
 	if Redis.Password == "" {
 		logrus.Fatal("REDIS_PASSWORD is not set")
-	}
-}
-
-func grpc() {
-	var err error
-
-	GRPC.Host = os.Getenv("GRPC_HOST")
-	if GRPC.Host == "" {
-		logrus.Fatal("GRPC_HOST is not set")
-	}
-
-	GRPC.Port, err = strconv.Atoi(os.Getenv("GRPC_PORT"))
-	if err != nil {
-		logrus.WithError(err).Fatal("failed to parse GRPC_PORT")
-	}
-
-	GRPC.Secret = os.Getenv("GRPC_SECRET")
-	if GRPC.Secret == "" {
-		logrus.Fatal("GRPC_SECRET is not set")
 	}
 }
 
