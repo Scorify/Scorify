0e297da20b540508101866900b57dbfabbf2d907
1nv8rZim <msf9542@rit.edu>
2024-11-07 22:50:52 -0500

    Add base container

--- src/pages/Statuses.tsx
+++ src/pages/Statuses.tsx
@@ -1,3 +1,20 @@
+import { Box, Container, Typography } from "@mui/material";
+
 export default function Statuses() {
-  return <></>;
+  return (
+    <Container component='main' maxWidth='md'>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component='h1' variant='h3' sx={{ marginBottom: "24px" }}>
+          Status Query
+        </Typography>
+      </Box>
+    </Container>
+  );
 }
