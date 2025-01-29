30a598ffe32ea91acf6045350efbb7b0e59bf8a5
1nv8rZim <msf9542@rit.edu>
2025-01-28 23:13:21 -0500

    baseline setup of `koth` cmd

--- pkg/cmd/koth/main.go
+++ pkg/cmd/koth/main.go
@@ -1,1 +1,21 @@
 package koth
+
+import (
+	"github.com/scorify/scorify/pkg/config"
+	"github.com/spf13/cobra"
+)
+
+var Cmd = &cobra.Command{
+	Use:     "koth",
+	Short:   "Start scoring koth worker",
+	Long:    "Start scoring koth worker",
+	Aliases: []string{"k", "worker", "w"},
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		config.InitKoth()
+	},
+
+	Run: run,
+}
+
+func run(cmd *cobra.Command, args []string) {
+}
