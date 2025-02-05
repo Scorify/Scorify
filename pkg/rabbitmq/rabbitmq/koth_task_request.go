cece109ec40fc3b55384b84d69756a164cb86d5c
1nv8rZim <msf9542@rit.edu>
2025-02-04 22:15:30 -0500

    document design of topic system for koth task routing

--- pkg/rabbitmq/rabbitmq/koth_task_request.go
+++ pkg/rabbitmq/rabbitmq/koth_task_request.go
@@ -5,6 +5,27 @@ const (
 	KothTaskRequestVhost = "koth_task_request_vhost"
 )
 
+/* ┌──────────────────────────────────────────────────────────────────┐
+ * │                             topic_key:                           │
+ * │                         koth.[check_name]                        │
+ * │                                                                  │
+ * │                 koth.check_1                                     │
+ * │                 koth.check_2                                     │
+ * │                      ┌─────────────►  Queue 1 ─────► Koth Minion │
+ * │                      │                                           │
+ * │                      │                                           │
+ * │                      │                                           │
+ * │                        koth.check_3                              │
+ * │ Server ─────► Exchange ────────────►  Queue 2 ─────► Koth Minion │
+ * │                                                                  │
+ * │                      │                                           │
+ * │                      │                                           │
+ * │                      │                                           │
+ * │                      └─────────────►  Queue 3 ─────► Koth Minion │
+ * │                 koth.check_4                                     │
+ * └──────────────────────────────────────────────────────────────────┘
+ */
+
 var (
 	// Permissions for minions in koth_task_request vhosts
 	KothTaskRequestConfigurePermissions   = regex(KothTaskRequestQueue)
