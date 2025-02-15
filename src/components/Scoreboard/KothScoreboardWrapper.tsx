1e74d02605733b4c1ee74cd88319d386ca74b333
1nv8rZim <msf9542@rit.edu>
2025-02-14 23:41:16 -0500

    implement `KothScoreboardWrapper`

---
+++ src/components/Scoreboard/KothScoreboardWrapper.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+import { Box } from "@mui/material";
+
+import { KothScoreboardQuery } from "../../graph";
+import { ScoreboardTheme } from "../../models";
+import KothScoreboard from "./KothScoreboard";
+import Scores from "./Scores";
+
+type props = {
+  theme: "dark" | "light";
+  data: KothScoreboardQuery["kothScoreboard"];
+  scoreboardTheme: ScoreboardTheme;
+};
+
+export default function KothScoreboardWrapper({
+  data,
+  theme,
+  scoreboardTheme,
+}: props) {
+  const [highlightedCheck, setHighlightedCheck] = useState<number | null>(null);
+  const [highlightedTeam, setHighlightedTeam] = useState<number | null>(null);
+
+  return (
+    <Box>
+      <KothScoreboard
+        theme={theme}
+        scoreboardData={data}
+        scoreboardTheme={scoreboardTheme}
+        highlightedRow={highlightedCheck}
+        highlightedColumn={highlightedTeam}
+        setHighlightedRow={setHighlightedCheck}
+        setHighlightedColumn={setHighlightedTeam}
+      />
+      <Scores
+        scores={data.scores}
+        highlightedTeam={null}
+        setHighlightedTeam={() => {}}
+      />
+    </Box>
+  );
+}
